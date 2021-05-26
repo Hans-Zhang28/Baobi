f08bb7483b34a3f563f5379a7a0b56e4654d0018
HansZhang <hans.zhang@vidyard.com>
2021-05-26 16:12:32 -0400

    success

---
+++ main.tf
@@ -0,0 +1,9 @@
+module "video_insights" {
+  source              = "./modules/boolean-feature"
+  project_key         = data.launchdarkly_project.default.key
+  feature_name        = "Video Insights"
+  feature_key         = "video_insights"
+  feature_description = "Enables Video Insights"
+  team                = "conversion"
+  temporary           = false
+}
